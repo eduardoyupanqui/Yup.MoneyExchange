fc0ebe18f6c54851d75479f7bedf26bb7064c73a
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2022-04-24 19:47:00 -0500

    update: Add some changes

--- src/yup-money-exchange/src/app/pages/exchange-rate/exchange-rate-routing.module.ts
+++ src/yup-money-exchange/src/app/pages/exchange-rate/exchange-rate-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 import { ExchangeRateComponent } from './container/exchange-rate.component';
 
 const routes: Routes = [
-  { path: '', component: ExchangeRateComponent }
+  { path: '', component: ExchangeRateComponent, canActivate:[AuthGuard] }
 ];
 
 @NgModule({
--- src/yup-money-exchange/src/app/pages/exchange/exchange-routing.module.ts
+++ src/yup-money-exchange/src/app/pages/exchange/exchange-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 import { ExchangeComponent } from './container/exchange.component';
 
 const routes: Routes = [
-  { path: '', component: ExchangeComponent}
+  { path: '', component: ExchangeComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
